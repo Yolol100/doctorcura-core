099abe19319ea9dd68df95aa6ddcd5a05ce484aa
Andrew Baeten <Andrew_Baeten@hotmail.com>
2026-09-09 18:06:23 +0200

    Harden WooCommerce login privacy scoping

--- includes/class-login-privacy.php
+++ includes/class-login-privacy.php
@@ -14,16 +14,27 @@ final class Login_Privacy {
     }
 
     public static function normalize_frontend_login_error( string $message ): string {
-        if ( is_admin() ) {
+        if ( is_admin() || empty( $_POST['login'] ) ) {
             return $message;
         }
 
-        if ( empty( $_POST['login'] ) || empty( $_POST['woocommerce-login-nonce'] ) ) {
+        $nonce = '';
+        if ( isset( $_REQUEST['woocommerce-login-nonce'] ) && is_scalar( $_REQUEST['woocommerce-login-nonce'] ) ) {
+            $nonce = sanitize_text_field( wp_unslash( (string) $_REQUEST['woocommerce-login-nonce'] ) );
+        } elseif ( isset( $_REQUEST['_wpnonce'] ) && is_scalar( $_REQUEST['_wpnonce'] ) ) {
+            $nonce = sanitize_text_field( wp_unslash( (string) $_REQUEST['_wpnonce'] ) );
+        }
+
+        if ( '' === $nonce || ! wp_verify_nonce( $nonce, 'woocommerce-login' ) ) {
             return $message;
         }
 
-        $username = isset( $_POST['username'] ) ? trim( (string) wp_unslash( $_POST['username'] ) ) : '';
-        $password = isset( $_POST['password'] ) ? (string) wp_unslash( $_POST['password'] ) : '';
+        $username = isset( $_POST['username'] ) && is_scalar( $_POST['username'] )
+            ? trim( (string) wp_unslash( $_POST['username'] ) )
+            : '';
+        $password = isset( $_POST['password'] ) && is_scalar( $_POST['password'] )
+            ? (string) wp_unslash( $_POST['password'] )
+            : '';
 
         if ( '' === $username || '' === $password ) {
             return $message;
