7b39c53ddfa6500a0ef3d0038cf5aa92c7c2721b
Andrew Baeten <Andrew_Baeten@hotmail.com>
2026-09-09 18:06:34 +0200

    Bump Core after login privacy hardening

--- doctorcura-core.php
+++ doctorcura-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: DoctorCura Core
  * Description: Core DoctorCura account, auth, checkout, order workflow, cancellation, and medical request features.
- * Version: 1.0.6
+ * Version: 1.0.7
  * Requires at least: 6.4
  * Requires PHP: 8.1
  * Author: OpenAI
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'DCAF_VERSION' ) ) {
-    define( 'DCAF_VERSION', '1.0.6' );
+    define( 'DCAF_VERSION', '1.0.7' );
 }
 
 if ( ! defined( 'DCAF_FILE' ) ) {
